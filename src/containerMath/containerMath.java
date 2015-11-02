d2aac30274f51c907ce595d6e2da96a9121b6d09
igor <igor@igorHP>
2015-11-02 23:41:22 +0600

    Merge origin/master
    Conflicts:
    	src/SenderController/SenderController.java

--- src/containerMath/containerMath.java
+++ src/containerMath/containerMath.java
@@ -7,6 +7,10 @@ package containerMath;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 /**
  *
  * @author igorbashka
@@ -16,6 +20,14 @@ public class containerMath {
     Reading excell file method
     */
     public void readFromExcel(){
-        OPCPackage pkg = OPCPackage.create(new File("my Path to File"));
+        try {
+            OPCPackage pkg = OPCPackage.create(new File("my Path to File"));
+            XSSFWorkbook book = new XSSFWorkbook(pkg);
+            Sheet sheet1 = book.getSheetAt(0);
+            Row row = sheet1.getRow(0);
+            Cell cell = row.getCell(2);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
