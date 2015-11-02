9cd042e03a859ccc84d1b0379e317823fe9ac7b9
igor <igor@localhost.localdomain>
2015-11-02 17:58:21 +0600

    FirstTries

--- src/containerMath/containerMath.java
+++ src/containerMath/containerMath.java
@@ -4,12 +4,18 @@
  * and open the template in the editor.
  */
 package containerMath;
-
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import java.io.File;
 /**
  *
  * @author igorbashka
  */
 public class containerMath {
-    
-    
+    /*
+    Reading excell file method
+    */
+    public void readFromExcel(){
+        OPCPackage pkg = OPCPackage.create(new File("my Path to File"));
+    }
 }
