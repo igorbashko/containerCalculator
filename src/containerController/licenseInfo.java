2eba93d48c214d8452ec242a163f127ff08a12db
igor <igor@localhost.localdomain>
2015-12-22 17:34:18 +0600

    FirstTries

--- src/containerController/licenseInfo.java
+++ src/containerController/licenseInfo.java
@@ -25,7 +25,9 @@ public class licenseInfo {
     private int runTimes;
     private String licenseKey;
     
-    private licenseInfo(String data){
+    /*Process data in input file for license checking purposes*/
+    public licenseInfo(String data){
+        variables = new String[3];
       InputStream stream = new ByteArrayInputStream(data.getBytes(StandardCharsets.UTF_8));
       BufferedReader rd = new BufferedReader(new InputStreamReader(stream));
       int index = 0;
@@ -39,4 +41,19 @@ public class licenseInfo {
             ex.printStackTrace();
         }
     }
-}
+    /*Get boolean value of was the software activated or not*/
+    public boolean getActivated(){
+        this.activated = Boolean.valueOf(variables[0].trim());
+        return activated;
+    }
+    /*Get to know how many times file was run under the demo license*/
+    public int getRunTimes(){
+        this.runTimes = Integer.parseInt(variables[1].trim());
+        return runTimes;
+    }
+    /*Get the license key*/
+    public String getKey(){
+        this.licenseKey = variables[2].trim();
+        return licenseKey;
+    }
+ }
---
+++ test/containerController/licenseInfoTest.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package containerController;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author igor
+ */
+public class licenseInfoTest {
+    private String testMessage = "FALSE\n1\nABC";
+    
+    public licenseInfoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getActivated method, of class licenseInfo.
+     */
+    @Test
+    public void testGetActivated() {
+        System.out.println("getActivated");
+        licenseInfo instance = new licenseInfo(testMessage);
+        boolean expResult = false;
+        boolean result = instance.getActivated();
+        assertEquals(expResult, result);
+       
+    }
+
+    /**
+     * Test of getRunTimes method, of class licenseInfo.
+     */
+    @Test
+    public void testGetRunTimes() {
+        System.out.println("getRunTimes");
+        licenseInfo instance = new licenseInfo(testMessage);
+        int expResult = 1;
+        int result = instance.getRunTimes();
+        assertEquals(expResult, result);
+       
+    }
+
+    /**
+     * Test of getKey method, of class licenseInfo.
+     */
+    @Test
+    public void testGetKey() {
+        System.out.println("getKey");
+        licenseInfo instance = new licenseInfo(testMessage);
+        String expResult = "ABC";
+        String result = instance.getKey();
+        assertEquals(expResult, result);
+       
+    }
+    
+}
