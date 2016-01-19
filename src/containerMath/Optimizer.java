1aaca823ad22e4cff1fb26b8e8018149aa239343
igor <igor@localhost.localdomain>
2016-01-19 13:48:48 +0600

    Created license.txt with computer unique information for key generating

---
+++ src/containerMath/Optimizer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package containerMath;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ *
+ * @author igor
+ */
+public class Optimizer {
+    private Stock stock;
+    private File sourceFile;
+    private List <Container> fourties;
+    private List <Container> twenties;
+    
+    public Optimizer(File file){
+        this.sourceFile = file;
+    }
+    
+    
+    
+}
